9f11532aaa84612389926b3c52bea4efb8512be4
Gideon kosgei <mr.gkosgei@gmail.com>
2021-11-10 09:29:20 +0300

    json

---
+++ json/join-from-integers-in-json-array.sql
@@ -0,0 +1,16 @@
+-- https://stackoverflow.com/questions/65365713/mysql-5-join-from-integers-in-json-array
+use adgg_uat;
+SELECT a.id,     
+	(
+		select json_arrayagg(r.label)
+		from core_master_list r
+		where json_contains(a.additional_attributes, cast(`r`.`value` as json), '$."249"') and
+		r.list_type_id = 8        
+    ) as data  
+FROM core_animal a WHERE a.id = 354269;
+
+ 
+
+
+
+
